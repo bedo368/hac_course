8774cf31400996abd1dd8402af2d2c2c61ecba52
Your Name <you@example.com>
2021-10-02 21:20:13 -0400

    NETWORK SCANER

--- NS_main.py
+++ NS_main.py
@@ -1,6 +1,25 @@
+#!/usr/bin/env python
+import re
+
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst= ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list  = scapy.srp(arp_request_broadcast , timeout=1 , verbose=False)[0]
+    print("ip" + "                                       " + "mac adress")
+    print("--------------------------------------------------------------")
+    list = []
+    for i in answered_list:
 
-scan("192.168.1.1/24")
+        print(i[1].psrc +"                               " + i[1].hwdst)
+        list.append({"ip" : i[1].psrc  , "mac_address": i[1].hwdst})
+
+
+    print("--------------------------------------------------------------")
+    return list
+
+
+
+print(scan("192.168.1.1/24"))
